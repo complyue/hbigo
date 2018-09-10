0b916179f3e555200c2e0739e056009c0fcb8dea
ComplYue <compl.yue@icloud.com>
2018-09-11 00:54:40 +0800

    clear test

--- hbi_test.go
+++ hbi_test.go
@@ -3,35 +3,13 @@ package hbi
 import (
 	"os"
 	"testing"
-	"text/template"
 )
 
 func TestMain(m *testing.M) {
-
-	ListenTCP(func() Context {
-		return NewContext()
-	}, "127.0.0.1:3232")
 
 	os.Exit(m.Run())
 }
 
 func TestGreeting(t *testing.T) {
-	hbic, err := MakeTCP(NewContext(), "127.0.0.1:3232")
-	defer CoDone(hbic.Co())
 
-	type Inventory struct {
-		Material string
-		Count    uint
-	}
-	sweaters := Inventory{"wool", 17}
-	tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
-	if err != nil {
-		panic(err)
-	}
-	err = tmpl.Execute(os.Stdout, sweaters)
-	if err != nil {
-		panic(err)
-	}
-
-	t.Log("Hello")
 }
